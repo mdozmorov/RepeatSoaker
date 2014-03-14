1459802264416089c4ee70bcf412e4465905ba5b
Cory Giles <mail@corygil.es>
2014-03-14 16:21:40 -0500

    Beginnings of Python/IntervalTree version.

---
+++ setup.py
@@ -0,0 +1,17 @@
+setup(
+    name="RepeatSoaker",
+    author="Mikhail Dozmorov, Cory Giles",
+    author_email="dozmorovm@omrf.org",
+    description="Post-process high-throughput sequencing alignments by removing reads falling in repetitive regions.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    license="AGPLv3+",
+    scripts="repeat-soaker"
+)
